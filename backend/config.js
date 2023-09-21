9f31e9666997b7b7429664ab69dee16949bb8460
jimmy <changye040@gmail.com>
2023-09-21 21:04:03 +0100

    integrate mongodb into proj, tested with mongoose lib

--- backend/config.js
+++ backend/config.js
@@ -1,1 +1,3 @@
 export const port = 8080;
+
+export const mongodbURL = "mongodb+srv://changye040:gdRGIQEXofcCElyp@cluster0.q6bfjrg.mongodb.net/?retryWrites=true&w=majority";
--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,23 @@
-import express from "express"
-import {port}  from "./config.js"
+import express, { request } from "express"
+import {port,mongodbURL}  from "./config.js"
+import mongoose from "mongoose";
 
 const app = express();
-app.listen(port,() =>{
-    console.log(`app running on port >> ${port}`);
+
+app.get('/',(reequest,response) =>{
+    console.log(request)
+    return response.status(200).send("home page")
 })
+// app.listen(port,() =>{
+//     console.log(`app running on port >> ${port}`);
+// })
+mongoose.connect(mongodbURL)
+        .then(()=>{
+            console.log('mongodb connected');
+            app.listen(port,() =>{
+                console.log(`app running on port >> ${port}`);
+        });
+    })
+        .catch((error) => {
+            console.log(error);
+        });
